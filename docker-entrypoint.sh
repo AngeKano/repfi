ed9f44725e8d00d37bfeed22347933b026e5ca9a
AngeKano <Angekano002@gmail.com>
2025-12-08 19:17:39 +0000

    entry-point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Generating Prisma client..."
-npx prisma generate
-
-echo "Pushing database schema..."
-npx prisma db push --skip-generate
+echo "Running database migrations..."
+npx prisma migrate deploy
 
 echo "Starting application..."
 exec node server.js
